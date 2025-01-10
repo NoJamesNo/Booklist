d655e91c52e7396dbe4a498169ccb4345f6be5ce
NoJamesNo <jamesnoh123@gmail.com>
2025-01-10 17:01:51 -0500

    fxed api

--- booklist/src/config/api.ts
+++ booklist/src/config/api.ts
@@ -1,7 +1,15 @@
 const API_BASE = import.meta.env.PROD ? import.meta.env.VITE_API_URL : '/api'
 
-export const fetchApi = (endpoint: string, options = {}) => {
+export const fetchApi = async (endpoint: string, options: RequestInit = {}) => {
   const url = `${API_BASE}${endpoint}`
-  console.log('Fetching from:', url)
-  return fetch(url, options)
+  console.log('Making request to:', url)
+  console.log('With headers:', options.headers)
+
+  return fetch(url, {
+    ...options,
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers
+    }
+  })
 }
